6e5aa6ab05cffc1dbf3962351ed94148de894fee
lijunwen13071121 <13237006076@163.com>
2021-01-27 11:46:26 +0800

    add qusetionPage

---
+++ src/pages/qusetion/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="qusetion-wrapper">移动端选择题组件</div>
+</template>
+<script>
+  export default {
+    name: 'Qusetion'
+  }
+</script>
+<style lang="less" scoped>
+  .qusetion-wrapper{
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
