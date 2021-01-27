492b00e16ab2fc43eaebe4087e2d95b2c366daf6
lijunwen13071121 <13237006076@163.com>
2021-01-27 10:51:00 +0800

    delete useless files

--- src/pages/homePage/index.vue
+++ src/pages/homePage/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <HelloWorld :msg="msg" />
     <Rend msg="render函数性能优化" />
     <div class="bubble-div">
       <Bubble content="test2" placement="top-left" :font-size="12"> 
@@ -11,14 +10,12 @@
 </template>
 
 <script>
-  import HelloWorld from '@/components/HelloWorld.vue'
   import Bubble from '@/components/Bubble.vue'
   import Rend from '@/components/render.vue'
 
   export default {
     name: 'Home',
     components: {
-      HelloWorld,
       Bubble,
       Rend
     },
